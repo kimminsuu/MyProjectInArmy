24c4a3b27810f14a629ea703014a3265d254ac6b
kimminsuu <49436152+kimminsuu@users.noreply.github.com>
2021-05-19 23:15:27 +0900

    Delete file.py

--- file.py
+++
@@ -1,7 +0,0 @@
-f=open("새파일.txt", 'w', encoding = "UTF-8")
-for i in range(1,11):
-  data = "%d번째 줄입니다.\n" %i
-  f.write(data)
-f.close()
-
-#open()함수의 앞쪽엔 주소, 두번째는 읽기(r), 쓰기(w) 등, encoding은 어떤 문자로 할건지
