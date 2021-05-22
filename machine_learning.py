8da3455f3899b13a351120945fcb159890d7e9ff
kimminsuu <49436152+kimminsuu@users.noreply.github.com>
2021-05-22 23:00:54 +0900

    Create machine_learning.py

---
+++ machine_learning.py
@@ -0,0 +1,20 @@
+#코드를 jupyter notebook으로 작성 가능
+import pandas
+import pandas as pd
+import tensorflow as tf
+tf.keras.layers.Dense(10)
+from tensorflow.keras.layers import Dense
+
+#1차원 리스트 생성
+
+x = [10,20,30,40,50,60,70,80,]
+#2차원 리스트
+
+x_2d = [[10,20,30,],[40,50,60,],]
+
+type(x)
+
+import numpy as np
+type(np.array(x))
+x_arr = np.array(x)
+x_arr.shape
