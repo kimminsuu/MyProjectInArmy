7dae9a7b1298ef8580aae9cf622902bdb82a9a69
kimminsuu <49436152+kimminsuu@users.noreply.github.com>
2021-06-03 23:35:50 +0900

    Update quantum_computer_2.py

--- quantum_computer_2.py
+++ quantum_computer_2.py
@@ -20,3 +20,18 @@ circuit.u(thet, phi, lamb, 0)
 circuit.measure([0], [0])
 circuit.draw()
 
+#Controlled-NOT Gate
+from qiskit import QuantumCircuit, execute, Aer
+from qiskit.visualization import plot_histogram
+circuit = QuantumCircuit(2, 2)
+circuit.h(0)
+circuit.cx(0, 1)
+circuit.measure([0, 1], [0, 1])
+circuit.draw()
+
+backend = Aer.get_backend('qasm_simulator')
+result = execute(circuit, backend).result()
+counts = result.get_counts()
+print(counts)
+plot_histogram(counts)
+
