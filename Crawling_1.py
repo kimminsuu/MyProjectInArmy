f1b2ff7250723186a7c6ca55a687957ea4078439
kimminsuu <49436152+kimminsuu@users.noreply.github.com>
2021-06-06 23:43:11 +0900

    Update Crawling_1.py

--- Crawling_1.py
+++ Crawling_1.py
@@ -6,4 +6,7 @@ driver.get(주소)
 elem = driver.find_element_by_name("q")
 elem.send_keys("key")
 elem.send_keys(Keys.RETURN)
-driver.find_
+driver.find_elements_by_css_selector(".rg_i.Q4LuWd")[0].click()
+#이미지 주소로 다운로드 받는다.
+
+#나머지는 구글에서 F12를 눌러 주소를 복사해 넣으며 크롤링하면 됨/
